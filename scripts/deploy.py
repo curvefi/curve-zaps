f872bc54cf0db93009a3e023288bae82d90a4b6a
macket <macket24@proton.me>
2023-06-19 17:57:48 +0400

    build: factory and eth implementation in deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -77,6 +77,8 @@ INIT_DATA = {
 
             [False, True, False, False, False],   # wBETH
         ],
+        "factory": "0xb9fc157394af804a3578134a6585c0dc9cc990d4",
+        "eth_implementation": "0x847ee1227A9900B73aEeb3a47fAc92c52FD54ed9",
     },
     "optimism": {
         "use_int128": [],
@@ -89,6 +91,8 @@ INIT_DATA = {
         "use_rate": [
             [False, True, False, False, False],   # wstETH
         ],
+        "factory": "0x2db0E83599a91b508Ac268a6197b8B14F5e72840",
+        "eth_implementation": "0x6F9fb833501f46CBE6f6A4b6Cf32C834E5A5e8C5",
     },
     "xdai": {
         "use_int128": [],
@@ -101,6 +105,8 @@ INIT_DATA = {
         "use_rate": [
             [True, False, False, False, False],   # rai
         ],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "polygon": {
         "use_int128": [],
@@ -113,6 +119,8 @@ INIT_DATA = {
         "use_rate": [
             [False, False, False, False, False],   # aave
         ],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "fantom": {
         "use_int128": [],
@@ -131,18 +139,24 @@ INIT_DATA = {
 
             [True, True, True, False, False],     # ib
         ],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "moonbeam": {
         "use_int128": [],
         "pool_type_addresses": [],
         "pool_types": [],
         "use_rate": [],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "kava": {
         "use_int128": [],
         "pool_type_addresses": [],
         "pool_types": [],
         "use_rate": [],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "arbitrum": {
         "use_int128": [],
@@ -155,12 +169,16 @@ INIT_DATA = {
         "use_rate": [
             [False, True, False, False, False],   # wstETH
         ],
+        "factory": "0xb17b674D9c5CB2e441F8e196a2f048A81355d031",
+        "eth_implementation": "0x6F9fb833501f46CBE6f6A4b6Cf32C834E5A5e8C5",
     },
     "celo": {
         "use_int128": [],
         "pool_type_addresses": [],
         "pool_types": [],
         "use_rate": [],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "avalanche": {
         "use_int128": [],
@@ -176,12 +194,16 @@ INIT_DATA = {
             [False, False, False, False, False],   # aave
             [False, False, False, False, False],   # aaveV3
         ],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
     "aurora": {
         "use_int128": [],
         "pool_type_addresses": [],
         "pool_types": [],
         "use_rate": [],
+        "factory": "0x0000000000000000000000000000000000000000",
+        "eth_implementation": "0x0000000000000000000000000000000000000000",
     },
 }
 
@@ -202,6 +224,8 @@ def main():
     pool_type_addresses = INIT_DATA[network_name]["pool_type_addresses"]
     pool_types = INIT_DATA[network_name]["pool_types"]
     use_rate = INIT_DATA[network_name]["use_rate"]
+    factory = INIT_DATA[network]["factory"]
+    eth_implementation = INIT_DATA[network]["eth_implementation"]
 
     use_int128 += [ZERO_ADDRESS] * (20 - len(use_int128))
     pool_type_addresses += [ZERO_ADDRESS] * (20 - len(pool_type_addresses))
@@ -211,11 +235,10 @@ def main():
     print(
         f"{network_name.upper()} CalcTokenAmount Zap constructor arguments:",
         encode(
-            ["address[20]", "address[20]", "uint8[20]", f"bool[{max_coins}][20]"],
-            [use_int128, pool_type_addresses, pool_types, use_rate],
+            ["address[20]", "address[20]", "uint8[20]", f"bool[{max_coins}][20]", "address", "address"],
+            [use_int128, pool_type_addresses, pool_types, use_rate, factory, eth_implementation],
         ).hex(),
     )
 
     txparams.update({'from': accounts[0]})
-    return CalcTokenAmountZap.deploy(use_int128, pool_type_addresses, pool_types, use_rate, txparams)
-
+    return CalcTokenAmountZap.deploy(use_int128, pool_type_addresses, pool_types, use_rate, factory, eth_implementation, txparams)
