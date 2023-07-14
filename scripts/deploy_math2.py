f22f586e8b82360242c244a905bf7ecf068a3e4e
macket <macket24@proton.me>
2023-07-14 12:39:31 +0400

    chore: deploy scripts for math2 and math3

---
+++ scripts/deploy_math2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from brownie import network
+from brownie import Math2, accounts
+
+
+def main():
+    txparams = {}
+    network_name = network.show_active()
+    if network_name == 'mainnet':
+        network_name = 'ethereum'
+    if network_name == 'ethereum':
+        accounts.load('curve-deployer')
+        txparams.update({'priority_fee': '2 gwei'})
+    elif not network_name.endswith("-fork"):
+        accounts.load('curve-deployer')
+
+    txparams.update({'from': accounts[0]})
+    return Math2.deploy(txparams)
